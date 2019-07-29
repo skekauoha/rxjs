d0b9f0ff572a7dd3d22c441f397501d45deb7f96
skekauoha <skekauoha@saltstack.com>
2019-07-28 22:14:39 -0600

    fix: update to about and home comp

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,35 +1,75 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
-import {catchError, delay, delayWhen, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-import {createHttpObservable} from '../common/util';
-import {Store} from '../common/store.service';
-
+import { Component, OnInit } from "@angular/core";
+import { Course } from "../model/course";
+import { interval, noop, Observable, of, throwError, timer } from "rxjs";
+import {
+  catchError,
+  delay,
+  delayWhen,
+  finalize,
+  map,
+  retryWhen,
+  shareReplay,
+  tap
+} from "rxjs/operators";
+import { createHttpObservable } from "../common/util";
+import { Store } from "../common/store.service";
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: "home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  beginnerCourses: Course[];
 
-    beginnerCourses$: Observable<Course[]>;
+  advancedCourses: Course[];
 
-    advancedCourses$: Observable<Course[]>;
+  constructor(private store: Store) {}
 
+  ngOnInit() {
+    const http$ = createHttpObservable("/api/courses");
+    const courses$ = http$.pipe(map(res => Object.values(res["payload"]))); // -> pipe allows you to chain multiple rx operators in order to produce a new observable
+    //    -> In this case we will take the http$ observable and pipe it into the rx map operator
 
-    constructor(private store:Store) {
-
-    }
-
-    ngOnInit() {
-
-        const courses$ = this.store.courses$;
-
-        this.beginnerCourses$ = this.store.selectBeginnerCourses();
-
-        this.advancedCourses$ = this.store.selectAdvancedCourses();
-
-    }
-
+    courses$.subscribe(
+      courses => {
+        this.beginnerCourses = courses.filter(
+          course => course.category == "BEGINNER"
+        );
+        this.advancedCourses = courses.filter(
+          course => course.category == "ADVANCED"
+        );
+      },
+      () => {},
+      () => console.log("complete")
+    );
+  }
 }
+
+// export class AboutComponent implements OnInit {
+//   beginnersCourses: Course[];
+
+//   constructor() {}
+
+//   ngOnInit() {
+//     const http$ = createHttpObservable("/api/courses");
+//     const courses$ = http$.pipe(map(res => Object.values(res["payload"]))); // -> pipe allows you to chain multiple rx operators in order to produce a new observable
+//     //    -> In this case we will take the http$ observable and pipe it into the rx map operator
+
+//     // This works, but you should avoid adding too much logic in the subscribe.
+//     // It's not scalable and we want to avoid callbacks or nested subscribes
+//     // considered an imperative approach
+//     courses$.subscribe(
+//       courses => {
+//         this.beginnersCourses = courses.filter(
+//           course => course.category == "BEGINNER"
+//         );
+//         this.beginnersCourses = courses.filter(
+//           course => course.category == "ADVANCED"
+//         );
+//       },
+//       () => {},
+//       () => console.log("complete")
+//     );
+//   }
+// }
