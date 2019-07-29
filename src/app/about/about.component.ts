2018ad475b9d6f3ad59db67bf5e95b5fe679a8be
skekauoha <skekauoha@saltstack.com>
2019-07-28 21:45:58 -0600

    rxjs: imperative design with subscribe

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { interval, timer, fromEvent, Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { createHttpObservable } from "../common/util";
+import { Course } from "../model/course";
 
 @Component({
   selector: "about",
@@ -9,6 +10,8 @@ import { createHttpObservable } from "../common/util";
   styleUrls: ["./about.component.css"]
 })
 export class AboutComponent implements OnInit {
+  beginnersCourses: Course[];
+
   constructor() {}
 
   ngOnInit() {
@@ -16,8 +19,18 @@ export class AboutComponent implements OnInit {
     const courses$ = http$.pipe(map(res => Object.values(res["payload"]))); // -> pipe allows you to chain multiple rx operators in order to produce a new observable
     //    -> In this case we will take the http$ observable and pipe it into the rx map operator
 
+    // This works, but you should avoid adding too much logic in the subscribe.
+    // It's not scalable and we want to avoid callbacks or nested subscribes
+    // considered an imperative approach
     courses$.subscribe(
-      courses => console.log(courses),
+      courses => {
+        this.beginnersCourses = courses.filter(
+          course => course.category == "BEGINNER"
+        );
+        this.beginnersCourses = courses.filter(
+          course => course.category == "ADVANCED"
+        );
+      },
       () => {},
       () => console.log("complete")
     );
