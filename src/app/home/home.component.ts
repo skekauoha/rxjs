d8fb9246e330c2967414e4dc6887914da15fb862
skekauoha <skekauoha@saltstack.com>
2019-07-28 22:15:27 -0600

    fix: remove comments

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -31,6 +31,9 @@ export class HomeComponent implements OnInit {
     const courses$ = http$.pipe(map(res => Object.values(res["payload"]))); // -> pipe allows you to chain multiple rx operators in order to produce a new observable
     //    -> In this case we will take the http$ observable and pipe it into the rx map operator
 
+    // This works, but you should avoid adding too much logic in the subscribe.
+    // It's not scalable and we want to avoid callbacks or nested subscribes
+    // considered an imperative approach
     courses$.subscribe(
       courses => {
         this.beginnerCourses = courses.filter(
@@ -45,31 +48,3 @@ export class HomeComponent implements OnInit {
     );
   }
 }
-
-// export class AboutComponent implements OnInit {
-//   beginnersCourses: Course[];
-
-//   constructor() {}
-
-//   ngOnInit() {
-//     const http$ = createHttpObservable("/api/courses");
-//     const courses$ = http$.pipe(map(res => Object.values(res["payload"]))); // -> pipe allows you to chain multiple rx operators in order to produce a new observable
-//     //    -> In this case we will take the http$ observable and pipe it into the rx map operator
-
-//     // This works, but you should avoid adding too much logic in the subscribe.
-//     // It's not scalable and we want to avoid callbacks or nested subscribes
-//     // considered an imperative approach
-//     courses$.subscribe(
-//       courses => {
-//         this.beginnersCourses = courses.filter(
-//           course => course.category == "BEGINNER"
-//         );
-//         this.beginnersCourses = courses.filter(
-//           course => course.category == "ADVANCED"
-//         );
-//       },
-//       () => {},
-//       () => console.log("complete")
-//     );
-//   }
-// }
