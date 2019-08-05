7f09f302e81afd36c52bdb2ddc7094c56287e113
skekauoha <skekauoha@saltstack.com>
2019-08-04 21:31:53 -0600

    rxjs-store-pattern

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,7 +1,18 @@
 import { Component, OnInit } from "@angular/core";
-import { of, concat, interval, merge } from "rxjs";
+import {
+  of,
+  concat,
+  interval,
+  merge,
+  Subject,
+  from,
+  BehaviorSubject,
+  AsyncSubject,
+  ReplaySubject
+} from "rxjs";
 import { map } from "rxjs/operators";
 import { createHttpObservable } from "../common/util";
+import { fromPromise } from "rxjs/internal-compatibility";
 
 @Component({
   selector: "about",
@@ -12,9 +23,29 @@ export class AboutComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    const http$ = createHttpObservable("/api/courses");
-    const sub = http$.subscribe(console.log);
+    // const http$ = createHttpObservable("/api/courses");
+    // const sub = http$.subscribe(console.log);
 
-    setTimeout(() => sub.unsubscribe(), 0);
+    // setTimeout(() => sub.unsubscribe(), 0);
+
+    // Subject is an observer and an observable
+    // BehaviorSubject can receive late subscribers values and sets a default value
+    // AsyncSubject will wait for complete to be called and then emit the last value
+    // ReplaySubject will replay the complete observable to late subscribers
+    const subject = new ReplaySubject();
+
+    const series$ = subject.asObservable();
+
+    series$.subscribe(val => console.log("early sub: ", val));
+
+    subject.next(1);
+    subject.next(2);
+    subject.next(3);
+    // subject.complete();
+
+    setTimeout(() => {
+      series$.subscribe(val => console.log("second sub: ", val));
+      subject.next(4);
+    }, 3000);
   }
 }
