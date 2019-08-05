6cfb6d64bb2fcaecaec89ff930353e6b5c8ac3a2
skekauoha <skekauoha@saltstack.com>
2019-08-04 19:12:43 -0600

    rxjs: forkjoin and debugging

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -11,22 +11,16 @@ import {
   debounceTime,
   distinctUntilChanged,
   startWith,
-  tap,
-  delay,
   map,
-  concatMap,
   switchMap,
-  withLatestFrom,
-  concatAll,
-  shareReplay,
-  throttle,
-  throttleTime
+  tap
 } from "rxjs/operators";
-import { merge, fromEvent, Observable, concat, interval } from "rxjs";
+import { fromEvent, Observable, forkJoin } from "rxjs";
 import { Lesson } from "../model/lesson";
 import { createHttpObservable } from "../common/util";
 import { Store } from "../common/store.service";
 import { searchLessons } from "../../../server/search-lessons.route";
+import { RxJsLoggingLevel, debug } from "../common/debug";
 
 @Component({
   selector: "course",
@@ -44,31 +38,44 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.courseId = this.route.snapshot.params["id"];
-    this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+    const course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+    const lesson$ = this.loadLessons();
+
+    // forkJoin, call all methods and will wait for the reponses to return in the order they were called.
+    // After all responses are returned, then continue.  --> forking the streams and joining them
+
+    forkJoin(course$, lesson$)
+      .pipe(
+        tap(([course, lessons]) => {
+          console.log("course: ", course);
+          console.log("lessons: ", lessons);
+        })
+      )
+      .subscribe();
   }
 
   ngAfterViewInit() {
-    // this.lessons$ = fromEvent<any>(this.input.nativeElement, "keyup").pipe(
-    //   map(event => event.target.value),
-    //   startWith(""),
-    //   debounceTime(400),
-    //   distinctUntilChanged(),
-    //   // switchMap cancels the previous observable and switches to the new observable
-    //   switchMap(search => this.loadLessons(search))
-    // );
+    this.lessons$ = fromEvent<any>(this.input.nativeElement, "keyup").pipe(
+      map(event => event.target.value),
+      startWith(""),
+      debounceTime(400),
+      distinctUntilChanged(),
+      // switchMap cancels the previous observable and switches to the new observable
+      switchMap(search => this.loadLessons(search))
+    );
 
-    fromEvent<any>(this.input.nativeElement, "keyup")
-      .pipe(
-        map(event => event.target.value),
-        startWith(""),
-        // debouncing is about waiting for a value to become stable
-        // debounceTime(400)
-        // throttle limits the output by limiting the number of values that can be emitted in a certain interval
-        throttle(() => interval(500)),
-        // throttleTime works as the same above
-        throttleTime(500)
-      )
-      .subscribe(console.log);
+    // fromEvent<any>(this.input.nativeElement, "keyup")
+    //   .pipe(
+    //     map(event => event.target.value),
+    //     startWith(""),
+    //     // debouncing is about waiting for a value to become stable
+    //     // debounceTime(400)
+    //     // throttle limits the output by limiting the number of values that can be emitted in a certain interval
+    //     throttle(() => interval(500)),
+    //     // throttleTime works as the same above
+    //     throttleTime(500)
+    //   )
+    //   .subscribe(console.log);
   }
 
   loadLessons(search = ""): Observable<Lesson[]> {
